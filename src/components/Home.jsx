85a34954607de26240bc6473522e2d86443f880e
Karthik Acharya <karthik.ach2608@gmail.com>
2024-07-01 20:58:01 +0530

    Added some styling to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,36 @@
-import { Box, Button, Heading } from "@chakra-ui/react";
+import { Box, Button, Heading, VStack, Text } from "@chakra-ui/react";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-
-    const navigate = useNavigate();
-
-
+  const navigate = useNavigate();
 
   return (
-    <Box>
-      <Heading>Welcome Home</Heading>
-      <Button onClick={()=>navigate('/products')}>View Products</Button>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+      bg="gray.50"
+      p={5}
+    >
+      <VStack spacing={6} textAlign="center">
+        <Heading as="h1" size="2xl" color="purple.700">
+          Welcome Home
+        </Heading>
+        <Text fontSize="lg" color="gray.600">
+          Discover our amazing products by clicking the button below.
+        </Text>
+        <Button
+          onClick={() => navigate('/products')}
+          colorScheme="purple"
+          size="lg"
+          px={8}
+          py={6}
+        >
+          View Products
+        </Button>
+      </VStack>
     </Box>
   );
 };
